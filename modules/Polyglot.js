0c9a051572284f8e74b151ad85c869848a3b673f
Yuzhe Wu <wuyuzhe10@gmail.com>
2017-03-06 10:23:26 +0100

    update

--- modules/Polyglot.js
+++ modules/Polyglot.js
@@ -2,8 +2,9 @@ var PolyglotNode = require('node-polyglot');
 var fs = require('fs');
 
 var Polyglot = function(language){
-	if(language != 'fr_FR'):
+	if(language != 'fr_FR') {
 		language = 'en_GB'
+	}
     var polyglot = new PolyglotNode();
     polyglot.locale(language);
     var contents = fs.readFileSync('./messages/' + language + '.json');
